4887b885c00e5d5285dff4164b318a7c1d74f6f2
vienvan <vienvonvan@gmail.com>
2019-01-18 12:38:14 -0800

    added validation on names

--- js/site-bundle.js
+++ js/site-bundle.js
@@ -1,12 +1,32 @@
+$(document).ready(function () {
+    $firstName = $("input[name=First_Name]");
+    $lastName = $("input[name=Last_Name]");
+    if ($firstName.val() == "" || $lastName.val() == "") {
+        $firstName.blur(function (event) {
+            event.target.checkValidity();
+        }).bind('invalid', function (event) {
+            setTimeout(function () { $(event.target).focus(); }, 50);
+        });
+        $lastName.blur(function (event) {
+            event.target.checkValidity();
+        }).bind('invalid', function (event) {
+            setTimeout(function () { $(event.target).focus(); }, 50);
+        });
+    }
+    console.log("ready!");
+});
+
 $("#mobileMenuLink a").on("click", function (e) {
     console.log("WOOOOO!"); 
     $("#mobileNav").toggleClass("menu-open")
 })    
 
 $("#guests-select").change(function () {
+    
     guests = $("#guests-select").val()
     console.log(guests)
     if (guests != "" && guests != "0") {
+        
         $("#main-form").toggle("slide");
         $("#guests-form").toggle("slide");
         if (guests == "1") {
@@ -14,38 +34,50 @@ $("#guests-select").change(function () {
             $("#plus-two").hide()
             $("#plus-three").hide()
             $("#plus-four").hide()
-            $("#plus-five").hide()
+            
+            makeRequired("Plus_one")
+            makeNotRequired("Plus_two")
+            makeNotRequired("Plus_three")
+            makeNotRequired("Plus_four")
+            
         }
         else if (guests == "2") {
             $("#plus-one").show()
             $("#plus-two").show()
             $("#plus-three").hide()
             $("#plus-four").hide()
-            $("#plus-five").hide()
+            makeRequired("Plus_one")
+            makeRequired("Plus_two")
+            makeNotRequired("Plus_three")
+            makeNotRequired("Plus_four")
         }
         else if (guests == "3") {
             $("#plus-one").show()
             $("#plus-two").show()
             $("#plus-three").show()
             $("#plus-four").hide()
-            $("#plus-five").hide()
+            
+            makeRequired("Plus_one")
+            makeRequired("Plus_two")
+            makeRequired("Plus_three")
+            makeNotRequired("Plus_four")
         }
         else if (guests == "4") {
             $("#plus-one").show()
             $("#plus-two").show()
             $("#plus-three").show()
             $("#plus-four").show()
-            $("#plus-five").hide()
-        }
-        else if (guests == "5") {
-            $("#plus-one").show()
-            $("#plus-two").show()
-            $("#plus-three").show()
-            $("#plus-four").show()
-            $("#plus-five").show()
+            makeRequired("Plus_one")
+            makeRequired("Plus_two")
+            makeRequired("Plus_three")
+            makeRequired("Plus_four")
         }
     } else {
         console.log("nothing happened");
+        makeNotRequired("Plus_one")
+        makeNotRequired("Plus_two")
+        makeNotRequired("Plus_three")
+        makeNotRequired("Plus_four")
     }
 
 
@@ -73,5 +105,19 @@ $('#rsvp-form').on('submit', function(e) {
   }
 
     
+  ).error(function (data) {
+      alert("Please make sure you have filled out all required (*) fields!")
+  }
+
   );
 })
+
+let makeRequired = function(prefix) {
+    $("select[name="+  prefix + "_entree]").prop("required", true)
+    $("input[name=" + prefix + "_name]").prop("required", true)
+}
+
+let makeNotRequired = function(prefix) {
+    $("input[name=" + prefix + "_name]").prop("required", false)
+    $("select[name=" + prefix + "_entree]").prop("required", false)
+}
